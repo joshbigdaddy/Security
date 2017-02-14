295bf2226ea35a712516402f718d097b870cc19e
Joshua Cifuentes <joshua.cifuentes.romero@hotmail.com>
2017-02-14 20:31:48 +0100

    Window structure prepared

--- HashingSecurity/src/com/ps1/window/Window.java
+++ HashingSecurity/src/com/ps1/window/Window.java
@@ -3,10 +3,31 @@ package com.ps1.window;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JTextPane;
+import javax.swing.JMenuBar;
+import java.awt.BorderLayout;
+import javax.swing.BoxLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import javax.swing.JPopupMenu;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.FlowLayout;
+import java.awt.Color;
+import javax.swing.JTextField;
+import java.awt.Scrollbar;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
 
 public class Window {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -36,8 +57,52 @@ public class Window {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.getContentPane().setForeground(Color.BLUE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnExamine = new JButton("Run");
+		btnExamine.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnExamine.setBounds(323, 215, 97, 25);
+		frame.getContentPane().add(btnExamine);
+		
+		textField = new JTextField();
+		textField.setBounds(54, 65, 335, 22);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(54, 131, 340, 22);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblInsertPathOf = new JLabel("Insert path of configuration file");
+		lblInsertPathOf.setBounds(54, 49, 257, 16);
+		frame.getContentPane().add(lblInsertPathOf);
+		
+		JLabel lblInsertLogsDirectory = new JLabel("Insert logs directory");
+		lblInsertLogsDirectory.setBounds(54, 112, 295, 16);
+		frame.getContentPane().add(lblInsertLogsDirectory);
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-
+	private static void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			private void showMenu(MouseEvent e) {
+				popup.show(e.getComponent(), e.getX(), e.getY());
+			}
+		});
+	}
 }
