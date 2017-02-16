e3098ceb5efc89d5d7645f24dbaf4b6186d2cf61
Joshua Cifuentes <joshua.cifuentes.romero@hotmail.com>
2017-02-16 16:57:12 +0100

    Repeated Comprobation

--- HashingSecurity/src/com/psi1/utils/ExecutionUtils.java
+++ HashingSecurity/src/com/psi1/utils/ExecutionUtils.java
@@ -6,23 +6,40 @@ import java.awt.event.ActionListener;
 import javax.swing.JLabel;
 import javax.swing.Timer;
 
+import com.psi1.config.Configuration;
+
 public class ExecutionUtils {
 	private static Timer t;
 	private static Integer hora = 0;
 	private static Integer minuto = 0;
 	private static Integer segundo = 0;
+	private static Configuration configuration;
+	private static Integer timerRepeater = 1000;
+	private static Integer repetitions;
 
 	public static void RunUtility(JLabel lblTimer) {
 		System.out.println("I am functioning");
-
-		t = new Timer(1000, clock(lblTimer));
+		if(t==null){
+		t = new Timer(timerRepeater, clock(lblTimer));
+		repetitions=0;
+		}
 		t.start();
 	}
+
 	public static void StopUtility() {
 		System.out.println("I am stopping");
 
-		
 		t.stop();
+	}
+
+	public static Configuration getConfiguration() {
+		return configuration;
+	}
+
+	public static void setConfiguration(Configuration c) {
+
+		configuration = c;
+
 	}
 
 	public static ActionListener clock(JLabel lblTimer) {
@@ -42,6 +59,11 @@ public class ExecutionUtils {
 					minuto = 0;
 				}
 				updateLabel();
+				repetitions++;
+				if((repetitions%(new Integer(getConfiguration().getTiempo())/timerRepeater))==0){
+					FileUtils.proveAll(getConfiguration());
+				}
+			
 			}
 
 			private void updateLabel() {
